53bb962abc9d9b550ae65f00ccd1a23188a33102
Bala <balasubramanian.radhakrishnan@harman.com>
2019-09-11 15:22:44 +0530

    removed omitempty

--- tfgen/cloudwatch.go
+++ tfgen/cloudwatch.go
@@ -1,18 +1,18 @@
 package tfgen
 
 type AwsCloudwatchMetricAlarm struct {
-	AlarmName               string         `yaml:"alarmName" hcl:"alarm_name,omitempty"`
-	ComparisonOperator      string         `yaml:"comparisonOperator" hcl:"comparison_operator,omitempty"`
-	EvaluationPeriods       string         `yaml:"evaluationPeriods" hcl:"evaluation_periods,omitempty"`
-	MetricName              string         `yaml:"metricName" hcl:"metric_name,omitempty"`
-	Namespace               string         `yaml:"namespace" hcl:"namespace,omitempty"`
-	Period                  string         `yaml:"period" hcl:"period,omitempty"`
-	Statistic               string         `yaml:"statistic" hcl:"statistic,omitempty"`
-	Threshold               string         `yaml:"threshold" hcl:"threshold,omitempty"`
-	AlarmDescription        string         `yaml:"alarmDescription" hcl:"alarm_description,omitempty"`
-	InsufficientDataActions *[]string      `yaml:"insufficientDataActions" hcl:"insufficient_data_actions,omitempty`
-	AlarmActions            *[]string      `yaml:"alarmActions" hcl:"alarm_actions,omitempty`
-	MetricQuery             *[]MetricQuery `yaml:"metricQuery" hcl:"metric_query,omitempty`
+	AlarmName               string        `yaml:"alarmName" hcl:"alarm_name,omitempty"`
+	ComparisonOperator      string        `yaml:"comparisonOperator" hcl:"comparison_operator,omitempty"`
+	EvaluationPeriods       string        `yaml:"evaluationPeriods" hcl:"evaluation_periods,omitempty"`
+	MetricName              string        `yaml:"metricName" hcl:"metric_name,omitempty"`
+	Namespace               string        `yaml:"namespace" hcl:"namespace,omitempty"`
+	Period                  string        `yaml:"period" hcl:"period,omitempty"`
+	Statistic               string        `yaml:"statistic" hcl:"statistic,omitempty"`
+	Threshold               string        `yaml:"threshold" hcl:"threshold,omitempty"`
+	AlarmDescription        string        `yaml:"alarmDescription" hcl:"alarm_description,omitempty"`
+	InsufficientDataActions []string      `yaml:"insufficientDataActions" hcl:"insufficient_data_actions`
+	AlarmActions            []string      `yaml:"alarmActions" hcl:"alarm_actions`
+	MetricQuery             []MetricQuery `yaml:"metricQuery" hcl:"metric_query`
 }
 
 type MetricQuery struct {
