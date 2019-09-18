5d5eeb244e407698aaf21d04f0ac53c31787f447
Bala <balasubramanian.radhakrishnan@harman.com>
2019-09-18 15:11:14 +0530

    updated type to string

--- tfgen/dns.go
+++ tfgen/dns.go
@@ -3,7 +3,7 @@ package tfgen
 type Route53Record struct {
 	ZoneID  string `yaml:"zoneId" hcl:"zone_id",omitempty`
 	Name    string `yaml:"name" hcl:"name,omitempty"`
-	Type    int64  `yaml:"type" hcl:"type,omitempty"`
+	Type    string  `yaml:"type" hcl:"type,omitempty"`
 	TTL     int64  `yaml:"ttl" hcl:"ttl,omitempty"`
 	Records string `yaml:"records" hcl:"records,omitempty"`
 	Alias   *Alias `yaml:"alias" hcl:"alias,block"`
